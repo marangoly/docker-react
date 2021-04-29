8504ce61f5993f3eeac915ba6ac8f2e31eb7b3c0
marangoly <marangolygeorge@gmail.com>
2021-04-29 15:54:29 +0100

    updated feature brach

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi there..............!!!
+          This is another branch
         </p>
         <a
           className="App-link"
